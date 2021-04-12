0c828256ace3313e481cb528a8a262fe6aae4d5b
Michael Gold <goldmichael@gmail.com>
2021-04-12 18:19:19 -0400

    fix: uncomment code

--- install-spacemacs.py
+++ install-spacemacs.py
@@ -40,38 +40,38 @@ with open(Path(base_path, 'config/config.json'), 'r') as f:
     config = json.load(f)
 
 
-# site_start_path = config["site_start_path"]
+site_start_path = config["site_start_path"]
 
-# print('Beginning file download with requests')
+print('Beginning file download with requests')
 
-# emacs_download_url = config["emacs_download_url"]
-# emacs_installer_file = home + '/Downloads/emacs-installer.exe'
-
-
-# request = requests.get(emacs_download_url)
-# with open(emacs_installer_file, 'wb') as f:
-#     f.write(request.content)
-
-# subprocess.run([emacs_installer_file])
+emacs_download_url = config["emacs_download_url"]
+emacs_installer_file = home + '/Downloads/emacs-installer.exe'
 
 
-# print('Copying additional scripts')
+request = requests.get(emacs_download_url)
+with open(emacs_installer_file, 'wb') as f:
+    f.write(request.content)
 
-# site_start_source = config["site_start_source"]
-# site_start_destination = "C:/Program Files/Emacs/x86_64/share/emacs/site-lisp/site-start.el"
+subprocess.run([emacs_installer_file])
 
-# dot_spacemacs_source = config["dot_spacemacs_source"]
-# dot_spacemacs_destination = home + "/.spacemacs"
 
-# add_home_path_to_site_start(base_path + "/" + site_start_source, home)
-# shutil.copyfile(site_start_source, site_start_destination)
-# shutil.copyfile(dot_spacemacs_source, dot_spacemacs_destination)
+print('Copying additional scripts')
 
-# print("Cloning spacemacs repo")
-# spacemacs_git_url = "https://github.com/syl20bnr/spacemacs"
-# spacemacs_install_path = home + "/.emacs.d"
+site_start_source = config["site_start_source"]
+site_start_destination = "C:/Program Files/Emacs/x86_64/share/emacs/site-lisp/site-start.el"
 
-# cloned_repo = git.repo.base.Repo.clone_from(spacemacs_git_url, spacemacs_install_path)
+dot_spacemacs_source = config["dot_spacemacs_source"]
+dot_spacemacs_destination = home + "/.spacemacs"
+
+add_home_path_to_site_start(base_path + "/" + site_start_source, home)
+shutil.copyfile(site_start_source, site_start_destination)
+shutil.copyfile(dot_spacemacs_source, dot_spacemacs_destination)
+
+print("Cloning spacemacs repo")
+spacemacs_git_url = "https://github.com/syl20bnr/spacemacs"
+spacemacs_install_path = home + "/.emacs.d"
+
+cloned_repo = git.repo.base.Repo.clone_from(spacemacs_git_url, spacemacs_install_path)
 
 
 # get source code pro fonts
@@ -82,8 +82,6 @@ request = requests.get(source_code_pro_download_url)
 with open(source_code_pro_download_file, 'wb') as f:
     f.write(request.content)
 
-
-# font_path = home + "/AppData/Local/Microsoft/Windows/Fonts/"
 font_path = home + "/Downloads/source_code_pro/"
 
 with ZipFile(source_code_pro_download_file, "r") as f:
