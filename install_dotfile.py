ba06f333e52e022e066fac6607210ad78e39b730
Michael Gold <goldmichael@gmail.com>
2021-04-12 23:03:36 -0400

    refactor: extract install_dot_spacemacs() into separate function

---
+++ install_dotfile.py
@@ -0,0 +1,3 @@
+from install_spacemacs import install_dot_spacemacs 
+
+install_dot_spacemacs()
--- install_spacemacs.py
+++ install_spacemacs.py
@@ -1,5 +1,5 @@
 from elevate import elevate
-from font_lib import install_font
+from font_helpers import install_font
 from os.path import dirname
 from os.path import expanduser
 from os.path import realpath
@@ -25,6 +25,9 @@ def install_emacs():
     subprocess.run([emacs_installer_file])
 
 def update_home_path_in_site_start_file(site_start_source, home):
+    '''
+    use the logged in user's home path instead of the one listed in the config file
+    '''
     home = Path(home).as_posix() + "/"
     src = '(set-home-dir "c:/users/goldm/")'
     target = '(set-home-dir "{}")'.format(home)
@@ -38,27 +41,35 @@ def update_home_path_in_site_start_file(site_start_source, home):
         for line in lines:
             outfile.write(line)
 
-def copy_additonal_config_files():
-    print('Copying additional config files')
+def install_site_start():
+    print('Installing site-start.el')
 
     site_start_source = config["site_start_source"]
 
     # emacs will look in this path to load this file on start
     site_start_destination = "C:/Program Files/Emacs/x86_64/share/emacs/site-lisp/site-start.el"
 
+    update_home_path_in_site_start_file(base_path + "/" + site_start_source, home)
+
+    shutil.copyfile(site_start_source, site_start_destination)
+
+def install_dot_spacemacs():
+    '''
+    copy `./config/.spacemacs` to `~/.spacemacs` 
+    '''
+    print('Installing.spacemacs')
     dot_spacemacs_source = config["dot_spacemacs_source"]
     dot_spacemacs_destination = home + "/.spacemacs"
-
-    update_home_path_in_site_start_file(base_path + "/" + site_start_source, home)
-    shutil.copyfile(site_start_source, site_start_destination)
+    
     shutil.copyfile(dot_spacemacs_source, dot_spacemacs_destination)
+
 
 def clone_spacemacs_github_repo():
     print("Cloning spacemacs repo")
     spacemacs_git_url = "https://github.com/syl20bnr/spacemacs"
     spacemacs_install_path = home + "/.emacs.d"
 
-    cloned_repo = git.repo.base.Repo.clone_from(spacemacs_git_url, spacemacs_install_path)
+    git.repo.base.Repo.clone_from(spacemacs_git_url, spacemacs_install_path)
 
 def install_source_code_pro_fonts():
     source_code_pro_download_url = config["source_code_pro_download_url"]
@@ -76,8 +87,15 @@ def install_source_code_pro_fonts():
                 f.extract(name, path=font_path)
                 install_font(font_path + name)
 
-# run in admin mode so we can install emacs and fonts
-elevate()
+def main():
+    # run in admin mode so we can install emacs and fonts
+    elevate()
+
+    install_emacs()
+    install_site_start()
+    install_dot_spacemacs()
+    clone_spacemacs_github_repo()
+    install_source_code_pro_fonts()
 
 base_path = dirname(realpath(__file__))
 home = expanduser("~")
@@ -85,7 +103,5 @@ home = expanduser("~")
 with open(Path(base_path, 'config/config.json'), 'r') as f:
     config = json.load(f)
 
-install_emacs()
-copy_additonal_config_files()
-clone_spacemacs_github_repo()
-install_source_code_pro_fonts()
+if __name__ == '__main__':
+    main()
