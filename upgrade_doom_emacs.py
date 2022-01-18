81e1f9e095600faa98d86bfbb5e5f52d0208f58e
Michael Gold <goldmichael@gmail.com>
2022-01-18 10:49:42 -0500

    fix: use remove_dir_if_exists

--- upgrade_doom_emacs.py
+++ upgrade_doom_emacs.py
@@ -1,26 +1,15 @@
 import os
-import shutil
-import stat
 import subprocess
-from install_doom_emacs import install_config_files, run_doom_env, run_doom_sync 
+from install_doom_emacs import install_config_files, install_custom_doom_cmd, remove_dir_if_exists, run_doom_env, run_doom_sync 
 from pathlib import PureWindowsPath
 
 home = os.path.expanduser("~")
 
+local_install = PureWindowsPath(os.path.join(home, '.emacs.d/.local/'))
 
-dir_path = PureWindowsPath(os.path.join(home, '.emacs.d/.local/'))
-
-print(dir_path)
-
-try:
-    os.chmod(dir_path, stat.S_IWUSR) #take ownership
-    os.chmod(dir_path, stat.S_IWRITE) #make writeable
-    shutil.rmtree(dir_path)
-except OSError as e:
-    print("Error: %s : %s" % (dir_path, e.strerror))
-
-
-subprocess.call(['doom', '-d', 'upgrade'], shell=True)
+remove_dir_if_exists(local_install)
+subprocess.call(['doom', 'upgrade', '-f'], shell=True)
+install_custom_doom_cmd()
 run_doom_env()
 install_config_files()
 run_doom_sync()
